cc7ce234a3f658a79ebed88b174b095c5aeb5959
Doychin94 <docho9497@gmail.com>
2024-12-11 15:15:04 +0200

    Add client feedback

--- src/js/modules/count.js
+++ src/js/modules/count.js
@@ -1,14 +1,17 @@
 $('.js-quantity').on('click', '.btn-increase', function (event) {
 	event.preventDefault();
 
+	const $input = $(this).closest('.checkbox').find('input');
 	const $countSpan = $(this).closest('.js-quantity').find('.checkbox__count');
 	let count = parseInt($countSpan.text());
 	count += 1;
 	$countSpan.text(count);
+	$input.attr('checked', true);
 });
 
 $('.js-quantity').on('click', '.btn-decrease', function (event) {
 	event.preventDefault();
+	const $input = $(this).closest('.checkbox').find('input')
 	const $countSpan = $(this).closest('.js-quantity').find('.checkbox__count');
 	let count = parseInt($countSpan.text());
 
@@ -18,4 +21,18 @@ $('.js-quantity').on('click', '.btn-decrease', function (event) {
 
 	count -= 1;
 	$countSpan.text(count);
+
+	if (count === 0) {
+		$input.attr('checked', false);
+		return;
+	}
 });
+
+$('.checkbox').on('click', 'label', function(event) {
+	event.preventDefault();
+
+	let isClicked = true;
+	$(this).siblings('input').attr('checked', isClicked);
+
+	isClicked = !isClicked
+})
