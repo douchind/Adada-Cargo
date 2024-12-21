42d23a9a4ddb0ddb0c01a09ae00f3be711647cd4
Doychin94 <docho9497@gmail.com>
2024-12-21 16:39:43 +0200

    Add popups

--- src/js/modules/form-validation.js
+++ src/js/modules/form-validation.js
@@ -1,13 +1,14 @@
 $('.js-form form').on('submit', function (event) {
 	let isValid = true;
+
 	const $form = $(this).closest('form');
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-	$form.find('[data-validate]').each(function(index, el) {
+	$form.find('[data-validate]').each(function (index, el) {
 		const $this = $(el);
 		const type = $this[0].type;
 
-		if(type === 'checkbox') {
+		if (type === 'checkbox') {
 			const $parent = $this.closest('.checkbox-alt');
 
 			if ($this[0].checked) {
@@ -18,10 +19,8 @@ $('.js-form form').on('submit', function (event) {
 			$parent.addClass('is-error');
 		}
 
-		if(type === 'text') {
+		if (type === 'text') {
 			const $parent = $this.closest('.form__controls');
-			console.log($this[0].value);
-
 
 			if ($this[0].value === '') {
 				$parent.addClass('is-error');
@@ -31,7 +30,18 @@ $('.js-form form').on('submit', function (event) {
 			$parent.removeClass('is-error');
 		}
 
-		if(type === 'email') {
+		if ($this[0].tagName.toLowerCase() === 'textarea') {
+			const $parent = $this.closest('.form__controls');
+
+			if ($this[0].value === '') {
+				$parent.addClass('is-error');
+				return;
+			}
+
+			$parent.removeClass('is-error');
+		}
+
+		if (type === 'email') {
 			const $parent = $this.closest('.form__controls');
 			const emailValue = $this[0].value.trim();
 
@@ -42,7 +52,7 @@ $('.js-form form').on('submit', function (event) {
 
 			$parent.removeClass('is-error');
 		}
-	})
+	});
 
 	isValid = $form.find('.is-error').length === 0 ? true : false;
 
